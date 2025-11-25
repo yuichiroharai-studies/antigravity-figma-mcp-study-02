90c3acadfe4a0d424393d742add4f9a510e9a40c
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2025-11-26 08:35:17 +0900

    feat: IPアドレスでサイトにアクセスできるようにする

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
